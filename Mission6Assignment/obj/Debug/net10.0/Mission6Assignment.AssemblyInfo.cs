a2e63e5dddec5162d92522f35ab44751ff0c67e3
bbates01 <bradybates01@gmail.com>
2026-02-27 20:19:35 -0700

    stuff

--- Mission6Assignment/obj/Debug/net10.0/Mission6Assignment.AssemblyInfo.cs
+++ Mission6Assignment/obj/Debug/net10.0/Mission6Assignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission6Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce69a45878b0647e39edad40ec8234cc83c9e26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f99fd4c1f63a73162894e350e04b82bc46b3ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission6Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission6Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
